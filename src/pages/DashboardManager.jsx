041c75d56137e6922fda9b7d6204ef81e1aa9126
Wilbert Waller <wilbert.waller@parsons.com>
2023-02-19 23:45:20 -0700

    Update editor & table config tabs

--- src/pages/DashboardManager.jsx
+++ src/pages/DashboardManager.jsx
@@ -1,38 +1,28 @@
 import { Settings, ViewComfy } from '@mui/icons-material'
-import { TabContext, TabList, TabPanel } from '@mui/lab'
-import { Box, Tab } from '@mui/material'
 import { Form, Formik } from 'formik'
-import { map } from 'lodash'
-import React, { useState } from 'react'
-import { createContext } from 'react'
+import { values } from 'lodash'
+import React, { createContext, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import * as yup from 'yup'
 import Controls from '../components/dashboard-editor/controls/Controls'
 import General from '../components/dashboard-editor/general/General'
 import Layout from '../components/dashboard-editor/layout/Layout'
+import Tabs from '../components/Tabs'
 import { saveDashboard } from '../store/reducers/dashboardReducer'
 import { selectDashboard } from '../store/selectors/dashboardSelector'
 
 export const EditorContext = createContext()
 
 export const EDITOR = {
-  GENERAL: 'General',
-  LAYOUT: 'Layout'
-}
-
-export const getEditorProps = key => {
-  switch (key) {
-    case EDITOR.GENERAL: return { element: <General />, icon: <Settings /> }
-    case EDITOR.LAYOUT: return { element: <Layout />, icon: <ViewComfy /> }
-    default: return {}
-  }
+  GENERAL: { element: <General />, icon: <Settings />, selected: 'General' },
+  LAYOUT: { element: <Layout />, icon: <ViewComfy />, selected: 'Layout' }
 }
 
 export default function DashboardManager() {
   const dispatch = useDispatch()
   const dashboard = useSelector(selectDashboard)
 
-  const [editor, setEditor] = useState(EDITOR.GENERAL)
+  const [editor, setEditor] = useState('General')
   const [dialogForm, setDialogForm] = useState(null)
 
   const validationSchema = yup.object({
@@ -67,41 +57,18 @@ export default function DashboardManager() {
           <Controls />
 
           { dashboard && (
-            <TabContext value={editor}>
-              <Box sx={{ border: 1, borderColor: 'divider' }}>
-                <TabList onChange={handleEditorClick} centered>
-                  { map(Object.values(EDITOR), (key, i) => {
-                    const { icon } = getEditorProps(key)
-                    return (
-                      <Tab
-                        key={`editor-${i}-tab`}
-                        icon={icon}
-                        iconPosition='start'
-                        label={key}
-                        value={key}
-                      />
-                    )
-                  }) }
-                </TabList>
-              </Box>
-              { map(Object.values(EDITOR), (key, i) => {
-                const { element } = getEditorProps(key)
-                return (
-                  <TabPanel
-                    key={`editor-${i}-content`}
-                    value={key}
-                    sx={{ padding: '2rem 0' }}
-                  >
-                    { element }
-                  </TabPanel>
-                )
-              }) }
-            </TabContext>
+            <Tabs
+              context='editor'
+              value={editor}
+              onChange={handleEditorClick}
+              list={values(EDITOR)}
+              centered
+            />
           ) }
         </Form>
       </Formik>
 
-    { dialogForm }
+      { dialogForm }
     </EditorContext.Provider>
   </>)
 }
