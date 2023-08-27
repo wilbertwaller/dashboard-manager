6a4d638b33818bc0b40cc45cb9dffb2d3ffca9eb
Wilbert Waller <wilbert.waller@gmail.com>
2023-08-27 17:45:22 -0600

    Create exercise banner component

---
+++ src/components/ExerciseBanner.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const bannerStyle = { color: 'red', display: 'flex', fontSize: '2.5em', justifyContent: 'center', width: '100%' }
+
+function ExerciseBanner() {
+  return (
+    <div style={bannerStyle}>Exercise Exercise Exercise</div>
+  )
+}
+
+export default ExerciseBanner
